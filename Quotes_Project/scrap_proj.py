d3fb37c084dfcb8137f511ab7a8140d7c730d8ad
guiOsorio <gosorio@sandiego.edu>
2021-09-01 20:29:05 -0700

    File logic added

--- Quotes_Project/scrap_proj.py
+++ Quotes_Project/scrap_proj.py
@@ -1,17 +1,15 @@
 # https://quotes.toscrape.com/
 import requests
 from random import choice
+# from csv import writer
 from bs4 import BeautifulSoup
 
 response = requests.get("https://quotes.toscrape.com")
 
-# for each author, create a dictionary with the author's name as the key, and within that, have 2 keys, "quotes" and "facts".
-    # the value for "quotes" should be a list with all the quotes containes in it
-    # the value for "facts" should be a list with all the facts about the author
+# add author facts - from each author's about page
 # create game logic and interface
 
 all_quotes = []
-authors = []
 pagenum = 1
 
 while True:
@@ -22,16 +20,23 @@ while True:
     for div in quote_divs:
         quote = div.select(".text")[0].get_text()
         author = div.select(".author")[0].get_text()
-        all_quotes.append(quote)
-        authors.append(author)
+        all_quotes.append([quote, author])
     pagenum += 1
     response = requests.get(f"https://quotes.toscrape.com/page/{pagenum}")
-    break
 
+# with open("quotes_data.csv", "w", newline="", encoding="utf-8") as csv_file:
+#     csv_writer = writer(csv_file)
+#     csv_writer.writerow(["quote", "author"])
+#     for quote in all_quotes:
+#         # Text
+#         text = quote[0]
+#         # Author
+#         author = quote[1]
+#         csv_writer.writerow([text, author])
 
-selected_quote = choice(all_quotes)
-
-print(authors)
+selected_quote_div = choice(all_quotes)
+selected_quote = selected_quote_div[0]
+selected_author = selected_quote_div[1]
 
 
 
